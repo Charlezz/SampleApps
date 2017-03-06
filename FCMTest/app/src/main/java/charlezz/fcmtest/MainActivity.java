b36d6fd9a02d43fc73b32943afe177d23be2bab5
Charles ok <oksisi213@gmail.com>
2017-03-06 15:51:33 +0900

    FCM Test

---
+++ FCMTest/app/src/main/java/charlezz/fcmtest/MainActivity.java
@@ -0,0 +1,43 @@
+package charlezz.fcmtest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GoogleApiAvailability.getInstance().makeGooglePlayServicesAvailable(this);
+
+        Button subscribeButton = (Button) findViewById(R.id.subscribeButton);
+        subscribeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseMessaging.getInstance().subscribeToTopic("news");
+                Toast.makeText(MainActivity.this, "subscribed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button logTokenButton = (Button) findViewById(R.id.logTokenButton);
+        logTokenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String token = FirebaseInstanceId.getInstance().getToken();
+                Toast.makeText(MainActivity.this, "token:" + token, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
