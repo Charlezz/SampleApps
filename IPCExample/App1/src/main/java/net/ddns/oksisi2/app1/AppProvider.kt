647c60c494a9392a008976b67cff84d43ec53ba2
Charles ok <shok@maxst.com>
2017-11-28 15:36:09 +0900

    IPC Example

--- IPCExample/App1/src/main/java/net/ddns/oksisi2/app1/AppProvider.kt
+++ IPCExample/App1/src/main/java/net/ddns/oksisi2/app1/AppProvider.kt
@@ -2,7 +2,6 @@ package net.ddns.oksisi2.app1
 
 import android.content.ContentProvider
 import android.content.ContentValues
-import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.UriMatcher
 import android.database.Cursor
@@ -17,23 +16,26 @@ import android.util.Log
  */
 
 
-
 class AppProvider : ContentProvider() {
     private val TAG = AppProvider::class.java.simpleName
 
-    lateinit var dbHelper:MyDBHelper
+    lateinit var dbHelper: MyDBHelper
 
     val MEMO = 0
     val MEMO_ID = 1
 
+    val PACKAGE_NAME = "net.ddns.oksisi2.app1"
 
     private var matcher = UriMatcher(UriMatcher.NO_MATCH)
+
+    init {
+        matcher.addURI(PACKAGE_NAME, "${MyDBHelper.TABLE_MEMO}", MEMO)
+        matcher.addURI(PACKAGE_NAME, "${MyDBHelper.TABLE_MEMO}/#", MEMO_ID)
+    }
 
     override fun onCreate(): Boolean {
         Log.e(TAG, "onCreate")
         dbHelper = MyDBHelper(context)
-        matcher.addURI(context.packageName, MyDBHelper.TABLE, MEMO)
-        matcher.addURI(context.packageName, "${MyDBHelper.TABLE}/#", MEMO_ID)
 
         return false
     }
@@ -41,8 +43,32 @@ class AppProvider : ContentProvider() {
     override fun query(uri: Uri, projection: Array<String>?, selection: String?, selectionArgs: Array<String>?, sortOrder: String?): Cursor? {
         Log.e(TAG, "query")
 
-        dbHelper.readableDatabase.query()y(MyDBHelper.TABLE,projection,selection, selectionArgs, )
-        return null
+        return when (matcher.match(uri)) {
+            MEMO -> dbHelper.writableDatabase.query(
+                    MyDBHelper.TABLE_MEMO,
+                    arrayOf(MyDBHelper.ID, MyDBHelper.NAME, MyDBHelper.CONTENT, MyDBHelper.TIMESTAMP),
+                    null,
+                    null,
+                    null,
+                    null,
+                    "${MyDBHelper.TIMESTAMP} DESC"
+            )
+
+            MEMO_ID -> dbHelper.writableDatabase.query(
+                    MyDBHelper.TABLE_MEMO,
+                    arrayOf(MyDBHelper.ID, MyDBHelper.NAME, MyDBHelper.CONTENT, MyDBHelper.TIMESTAMP),
+                    "${MyDBHelper.ID} = ?",
+                    arrayOf(uri.lastPathSegment),
+                    null,
+                    null,
+                    "${MyDBHelper.TIMESTAMP} DESC"
+            )
+
+            else -> {
+                IllegalArgumentException("This is an Unkown URI ${uri}")
+                return null
+            }
+        }
     }
 
     override fun getType(uri: Uri): String? {
@@ -52,47 +78,56 @@ class AppProvider : ContentProvider() {
 
     override fun insert(uri: Uri, values: ContentValues?): Uri? {
         Log.e(TAG, "insert")
-        dbHelper.writableDatabase.insert(MyDBHelper.TABLE, null, values)
+        Log.e(TAG,"saved:${dbHelper.writableDatabase.insert(MyDBHelper.TABLE_MEMO, null, values)}")
         return null
     }
 
-    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
-        Log.e(TAG, "delete")
-        return 0
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int = when (matcher.match(uri)) {
+        MEMO_ID -> dbHelper.writableDatabase.delete(MyDBHelper.TABLE_MEMO, "${MyDBHelper.ID} = ?", arrayOf(uri.lastPathSegment))
+        else -> {
+            IllegalArgumentException("This is an Unkown URI ${uri}")
+            0
+        }
     }
+
 
     override fun update(uri: Uri, values: ContentValues?, selection: String?, selectionArgs: Array<String>?): Int {
         Log.e(TAG, "update")
-        return 0
+        return when (matcher.match(uri)) {
+            MEMO_ID -> dbHelper.writableDatabase.update(MyDBHelper.TABLE_MEMO, values, "${MyDBHelper.ID} = ?", arrayOf(uri.lastPathSegment))
+            else -> 0
+        }
     }
 }
 
-class MyDBHelper(context:Context) : SQLiteOpenHelper(context, DB_NAME, null, DB_VERSION){
+class MyDBHelper(context: Context) : SQLiteOpenHelper(context, DB_NAME, null, DB_VERSION) {
+
+    val TAG = MyDBHelper::class.java.simpleName
 
     companion object {
-        val DB_NAME = "forecast.db"
+        val DB_NAME = "MyDB.db"
         val DB_VERSION = 1
 
-        val TABLE = "Memo"
+        val TABLE_MEMO = "Memo"
 
-        val ID:String = "_id"
-        val NAME:String = "Name"
-        val CONTENT:String = "String"
-        val TIMESTAMP:String = "Timestamp"
+        val ID: String = "_id"
+        val NAME: String = "Name"
+        val CONTENT: String = "String"
+        val TIMESTAMP: String = "Timestamp"
 
     }
 
     val DATABASE_CREATE =
-            "CREATE TABLE if not exists " + TABLE + " (" +
+            "CREATE TABLE if not exists ${TABLE_MEMO} (" +
                     "${ID} integer PRIMARY KEY autoincrement," +
-                    "${NAME} text,"+
-                    "${CONTENT} text,"+
-                    "${TIMESTAMP} integer" +
+                    "${NAME} text," +
+                    "${CONTENT} text," +
+                    "${TIMESTAMP} TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
                     ")"
 
     override fun onCreate(db: SQLiteDatabase?) {
+        Log.e(TAG,"onCreate")
         db?.execSQL(DATABASE_CREATE)
-
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
--- IPCExample/App1/src/main/java/net/ddns/oksisi2/app1/MainActivity.kt
+++ IPCExample/App1/src/main/java/net/ddns/oksisi2/app1/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
     val TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
