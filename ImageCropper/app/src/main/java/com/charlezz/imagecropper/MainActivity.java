128f359eff4f27d8f575843358a09bcc81c285a8
Charles <oksisi213@gmail.com>
2017-12-11 23:11:24 +0900

    no message

---
+++ ImageCropper/app/src/main/java/com/charlezz/imagecropper/MainActivity.java
@@ -0,0 +1,48 @@
+package com.charlezz.imagecropper;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.util.ArrayList;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        new TedPermission(this)
+                .setPermissions(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .setPermissionListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted() {
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+
+                    }
+                })
+                .check();
+
+
+
+    }
+
+    @OnClick(R.id.pick)
+    void onPickClick(){
+        CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .start(this);
+    }
+}
